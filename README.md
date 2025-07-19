de5059a58725e5040f8d6a7006274c2704721bb5
Jeyfferson <jeyffesilv@gmail.com>
2025-07-19 11:21:06 -0300

    atualizando os leads

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # tvboxpay
+# tvboxpay
--- js/leadForm.js
+++ js/leadForm.js
@@ -6,14 +6,14 @@ document.getElementById('leadForm').addEventListener('submit', function(e) {
   const celular = document.getElementById('celular').value;
 
   fetch('https://script.google.com/macros/s/AKfycbyOZfOD9r2R0dhK-WQZRRb8VPizDzzzFpSusoLc7mj8cfVezmgYA_3QXkCmGCUcD6Wi/exec', {
-    method: 'POST',
-    body: JSON.stringify({ nome, email, celular }),
-    headers: { 'Content-Type': 'application/json' }
-  })
-  .then(res => res.json())
-  .then(data => {
-    document.getElementById('leadForm').reset();
-    document.getElementById('successMessage').style.display = 'block';
-  })
-  .catch(err => console.error('Erro ao enviar lead:', err));
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ nome, email, celular })
+})
+.then(() => {
+  document.getElementById('leadForm').reset();
+  document.getElementById('successMessage').style.display = 'block';
+})
+.catch(err => console.error('Erro ao enviar lead:', err));
+
 });
